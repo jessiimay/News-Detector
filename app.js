3e74f1bb5adaf32631f18bb239207f8a079bc547
Wence-May <10165300206@stu.ecnu.edu.cn>
2020-05-17 16:49:06 +0800

    bug1

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//-----------------dependencies------------------
+/////////////////////////////////dependency////////////////////////////////
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
@@ -8,10 +8,10 @@ var consolidate = require('consolidate');
 var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();//创建server
-var handlebars = require('express3-handlebars').create({ defaultlayout:'main'});
+var handlebars = require('express3-handlebars').create({ defaultlayout: 'main' });
 
 
-//------------------setting-----------------------
+////////////////////////////////////setting//////////////////////////////////
 app.set('port', process.env.PORT || 3000);
 // app.engine('html', consolidate.ejs);
 app.engine('handlebars', handlebars.engine);
@@ -19,81 +19,96 @@ app.set('view engine', 'handlebars');
 app.set('views', __dirname + '/public');
 
 
-//------------------midwares----------------------
-app.use(function(req,res,next){
-    res.locals.showTests = app.get('env') != 'production' && req.query.showTests ==='1';
+////////////////////////////////////midware//////////////////////////////////
+app.use(function (req, res, next) {
+    res.locals.showTests = app.get('env') != 'production' && req.query.showTests === '1';
     next();
 });
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded());
 
+////////////////////////////////////request//////////////////////////////////
+///////////////////////pass params by local variables////////////////////////
+var Request = {
+    title : '',
+    kw : '',
+    content : ''
+}
 
-// var news = [];
-//------------------  routs-----------------------
+//////////////////////////////////////rout///////////////////////////////////
 //服务器主页输出
-app.get('/', async function (req, res) {
+app.get('/', function (req, res) {
+    Request.title = req.query.title;
+    Request.kw = req.query.kw;
+    Request.content = req.query.content;
+    console.log(Request.title);
+    console.log(Request.kw);
+    console.log(Request.content);
     res.render('home');
+    res.redirect('/news');
     // res.send(req.body);
-    title = req.body.title;
-    kw = req.body.kw;
-    content = req.body.content;
-    // res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-    try {
-        if ((title == undefined) && (req.body !== undefined)) {
-            res.send(req.body + "title is required to search");//title is required to search
-            console.log("!!!!fail value=");
-        }
-        else {
-            var select_Sql = "select title,author,publish_date,source_name,content from fetches where (title like '%" +
-                title + "%' or keywords like '%" + kw + "%') and content like '%" + content + "%'";
-            if (kw == undefined && content == undefined)
-                var select_Sql = "select title,author,publish_date,source_name,content from fetches where title like '%" +
-                    title + "%'";
-            else if (kw == undefined)
-                var select_Sql = "select title,author,publish_date,source_name,content from fetches where title like '%" +
-                    title + "%' and content like '%" + content + "%'";
-            else if (content == undefined)
-                var select_Sql = "select title,author,publish_date,source_name,content from fetches where title like '%" +
-                    title + "%' and keywords like '%" + kw + "%'";
 
-
-            let value =await mysql.promise_query(select_Sql, function (qerr, vals, fields) {
-                console.log("Input: " + JSON.stringify(req.body));
-            });
-
-            news = value;
-            res.redirect('/news');
-        }
-    } catch (err) {
-        console.log(err);
-    }
-    res.end();
 });
 
 app.post('/news', function (req, res) {
     res.render('news', news);
-    res.send(news);
+    console.log(Request.title);
+    console.log(Request.kw);
+    console.log(Request.content);
+    // res.send(news);
+    // res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    // try {
+    //     if ((title == undefined) && (req.body !== undefined)) {
+    //         res.send(req.body + "title is required to search");//title is required to search
+    //         console.log("!!!!fail value=");
+    //     }
+    //     else {
+    //         var select_Sql = "select title,author,publish_date,source_name,content from fetches where (title like '%" +
+    //             title + "%' or keywords like '%" + kw + "%') and content like '%" + content + "%'";
+    //         if (kw == undefined && content == undefined)
+    //             var select_Sql = "select title,author,publish_date,source_name,content from fetches where title like '%" +
+    //                 title + "%'";
+    //         else if (kw == undefined)
+    //             var select_Sql = "select title,author,publish_date,source_name,content from fetches where title like '%" +
+    //                 title + "%' and content like '%" + content + "%'";
+    //         else if (content == undefined)
+    //             var select_Sql = "select title,author,publish_date,source_name,content from fetches where title like '%" +
+    //                 title + "%' and keywords like '%" + kw + "%'";
+
+
+    //         let value =await mysql.promise_query(select_Sql, function (qerr, vals, fields) {
+    //             console.log("Input: " + JSON.stringify(req.body));
+    //         });
+
+    //         news = value;
+
+    //     }
+    // } catch (err) {
+    //     console.log(err);
+    // }
+    // res.end();
 });
 
 
 
 
-//--------------------error---------------------------
-app.use(function(req, res){
+////////////////////////////////////error//////////////////////////////////
+app.use(function (req, res) {
     res.type('text/plain');
     res.status(404);
     res.send('404 - Not Found');
 });
-app.use(function(err, req, res, next){
+app.use(function (err, req, res, next) {
     console.error(err.stack);
     res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.write('500 - Server Error');
+    res.end();
 });
 
 
 
-// ------------------server---------------------------
+////////////////////////////////////server//////////////////////////////////
 var server = app.listen(app.get('post'), "127.0.0.1", function () {//指跑在哪个端口
 
     var host = server.address().address
