c669846bab468caaebbdc757adfe70998ea14ca6
wucy <741882354@qq.com>
2020-06-15 07:40:41 +0800

    注册修改

--- app.js
+++ app.js
@@ -132,14 +132,15 @@ app.post('/login',async function(req, res){
 app.post('/register',async function(req, res){
     username = req.body.username
     password = req.body.password
-    var select_Sql = "select passwd from Account where username =" + username + ";";
+    var select_Sql = "select passwd from Account where username =" + username + ";"
     var num = 0
+    let value = await mysql.promise_query(select_Sql, function () {})
     for (var i in value) {
         num += 1;
     }
     if(num == 0){
-        var select_Sql = "insert into Account(username,passwd) values("+username+","+"password"+")";
-        let value = await mysql.promise_query(select_Sql, function () {});
+        var select_Sql = "insert into Account(username,passwd) values("+username+","+password+")"
+        let value = await mysql.promise_query(select_Sql, function () {})
         res.send('200')
     }else{
         res.send('500')
